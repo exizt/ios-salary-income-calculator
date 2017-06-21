7cc2a2ce8dffa030c7e63c2cf9babd3a5cca0fa5
SH.Hong <sh.hong@SHHongs-Mac.local>
2017-06-22 07:23:09 +0900

    Initial Commit

---
+++ SalaryIncomeCalculator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SalaryIncomeCalculator
+//
+//  Created by SH.Hong on 2017. 6. 22..
+//  Copyright © 2017년 SH.Hong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
