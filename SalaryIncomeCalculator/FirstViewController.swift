0ced1eace609fbea862cb866a8e16725b601011c
SH.Hong <sh.hong@SHHongs-Mac.local>
2017-06-24 19:49:53 +0900

    계산하기 버튼 없애고, 실시간 계산 방식으로 전환.
    가로세로 방향 전환에 맞춘 디자인 변경 (대체적으로 StackView 적용)

--- SalaryIncomeCalculator/FirstViewController.swift
+++ SalaryIncomeCalculator/FirstViewController.swift
@@ -21,6 +21,9 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         
         viewDidLoad_keyboardDone()
 
+        inMoney.addTarget(self, action: #selector(self.textFieldDidChanged_inMoney), for: .editingChanged)
+        inTaxFree.addTarget(self, action: #selector(self.textFieldDidChange_inTaxFree), for: .editingChanged)
+        iSegmentedAnnual.addTarget(self, action: #selector(self.iSegmentedAnnual_userChanged), for: .valueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,43 +31,31 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-    func viewDidLoad_keyboardDone()
+    /**
+    * 계산 메서드. 이 메서드로 호출해주자.
+    */
+    func loadCalculation()
     {
-        let toolBar = UIToolbar()
-        toolBar.sizeToFit()
+        guard let incomeMoney = Double((inMoney?.text!)!) else {
+            return
+        }
         
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        // 금액 입력이 10만원 이내면 계산안함
+        if(incomeMoney < 10 * 0000){
+            return
+        }
         
-        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.doneClicked))
-        
-        toolBar.setItems([flexibleSpace, doneButton], animated: false)
-        
-        //textfield 가 필요하면 여기에 추가
-        inMoney.inputAccessoryView = toolBar
-        inTaxFree.inputAccessoryView = toolBar
-    }
-    func doneClicked()
-    {
-        view.endEditing(true)
+        // 계산 메서드 호출
+        calculate()
     }
     
-    @IBAction func textFieldMoney(_ sender: UITextField) {
-        guard let incomeMoney = Double((sender.text!)) else {
-            return
-        }
-        
-        if(incomeMoney < 100000)
-        {
-            return
-        }
-        
-        if(incomeMoney>=10000000){
-            self.iSegmentedAnnual.selectedSegmentIndex = 0
-        } else {
-            self.iSegmentedAnnual.selectedSegmentIndex = 1
-        }
-    }
-    @IBAction func calculate(_ sender: UIButton) {
+    
+    /**
+    * 계산 메서드.
+    * 가급적 바로 호출하지 마시고, loadCalculation 메서드로 호출해주세요.
+    * 테스트 할 시에만 직접 호출 하세요.
+    */
+    func calculate() {
         guard let incomeMoney = Double((inMoney?.text!)!) else {
             return
         }
@@ -93,8 +84,85 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         incomeTax = calculator.getIncomeTax()
         
         resultDetail.text = String(format:"국민연금 : %@ 원 \r\n건강보험 : %@ 원 \r\n요양보험 : %@ 원 \r\n고용보험 : %@ 원 \r\n소득세 : %@ 원 \r\n지방세 : %@ 원",formatCurrency(insurance.nationalPension),formatCurrency(insurance.healthCare),formatCurrency(insurance.longTermCare),formatCurrency(insurance.employmentCare),formatCurrency(incomeTax.incomeTax),formatCurrency(incomeTax.localTax))
-
     }
+    
+    /**
+    * 금액 입력 시 메서드
+    */
+    func textFieldDidChanged_inMoney(_ sender: UITextField) {
+        guard let incomeMoney = Double((sender.text!)) else {
+            return
+        }
+        
+        if(incomeMoney < 100000)
+        {
+            return
+        }
+        
+        if(incomeMoney>=10000000){
+            self.iSegmentedAnnual.selectedSegmentIndex = 0
+        } else {
+            self.iSegmentedAnnual.selectedSegmentIndex = 1
+        }
+        
+        // 계산 호출
+        loadCalculation()
+    }
+    
+    /**
+     * 비과세 입력 시 메서드
+     */
+    func textFieldDidChange_inTaxFree(_ sender: UITextField){
+        let isDebug = true
+        
+        if(isDebug){
+            print("Testing!")
+            guard let text = Double((sender.text!)) else {
+                return
+            }
+            print(text)
+        }
+        
+        // 계산 호출
+        loadCalculation()
+    }
+    
+    func iSegmentedAnnual_userChanged(_ sender: UISegmentedControl){
+        print("Segmented 변경됨")
+    }
+    
+    /**
+     * 키보드 바로 위에 [Done] 추가하는 메서드
+     */
+    func viewDidLoad_keyboardDone()
+    {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.keyboard_doneClicked))
+        
+        toolBar.setItems([flexibleSpace, doneButton], animated: false)
+        
+        //textfield 가 필요하면 여기에 추가
+        inMoney.inputAccessoryView = toolBar
+        inTaxFree.inputAccessoryView = toolBar
+    }
+    
+    
+    /**
+     * 키보드 바로 위의 [Done] 클릭시 에 행동하는 메서드
+     */
+    func keyboard_doneClicked()
+    {
+        view.endEditing(true)
+    }
+    
+    /**
+    * 2000 (Double) -> 2,000 (String)
+    * 숫자를 금액처럼 , 넣고 변경해주는 메서드
+    */
     func formatCurrency(_ value: Double)->String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
--- SalaryIncomeCalculator/SecondViewController.swift
+++ SalaryIncomeCalculator/SecondViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    @IBOutlet weak var labelDescription: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelDescription.text = "이 앱은 연봉, 월급 지급액을 기준으로 월 실수령액 을 계산하는 앱입니다. 이번 버전은 2017년 2월 기준으로 작성되었습니다. 이후 세율 변경, 근로소득세 계산법 변경 으로 인한 오차가 발생할 수 있습니다. 물론 업데이트는 계속되어집니다.\r\n 가족수, 자녀수 등의 옵션을 다음 업데이트에서 적용될 예정입니다. "
     }
 
     override func didReceiveMemoryWarning() {
